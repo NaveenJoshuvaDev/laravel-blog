a796f7e5c6e3b4ca28471dd0f4d9b667b175dbec
naveenjoshuvdev <njworkholic@gmail.com>
2024-02-03 18:06:17 +0530

    added front end blog template

--- laravel-10-blog-new/app/Filament/Resources/PostResource.php
+++ laravel-10-blog-new/app/Filament/Resources/PostResource.php
@@ -19,13 +19,20 @@ class PostResource extends Resource
 {
     protected static ?string $model = Post::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static ?string $navigationGroup ='Content';
 
     public static function form(Form $form): Form
     {
         return $form
+        ->schema([
+            Forms\Components\Card::make()
+                
             ->schema([
-                Forms\Components\TextInput::make('title')
+                Forms\Components\Grid::make(2)
+                ->schema([
+                    Forms\Components\TextInput::make('title')
                     ->required()
                     ->maxLength(2048)
                     ->reactive()
@@ -35,36 +42,43 @@ class PostResource extends Resource
                 Forms\Components\TextInput::make('slug')
                     ->required()
                     ->maxLength(2048),
-                Forms\Components\TextInput::make('thumbnail')
-                  
-                    ->maxLength(2048),
-                Forms\Components\Textarea::make('body')
+                ])
+                ,
+              
+                Forms\Components\RichEditor::make('body')
                     ->required(),
                 Forms\Components\Toggle::make('active')
                     ->required(),
                 Forms\Components\DateTimePicker::make('published_at')
-                    ->required(),
-                Forms\Components\Select::make('user_id')
-                    ->relationship('user', 'name')
-                    ->required(),
-            ]);
+                    //->required(),
+               
+                    ])->columnSpan(8),
+
+                    Forms\Components\Card::make()
+                    ->schema([
+                        Forms\Components\FileUpload::make('thumbnail'),
+                        Forms\Components\Select::make('category_id')
+                        ->multiple()
+                        ->relationship('categories', 'title')
+                        ->required(),
+                    ])->columnSpan(4),
+            ])->columns(12);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
+                Tables\Columns\ImageColumn::make('thumbnail'),
                 Tables\Columns\TextColumn::make('title'),
-                Tables\Columns\TextColumn::make('slug'),
-                Tables\Columns\TextColumn::make('thumbnail'),
-                Tables\Columns\TextColumn::make('body'),
+               
+               
                 Tables\Columns\IconColumn::make('active')
                     ->boolean(),
                 Tables\Columns\TextColumn::make('published_at')
                     ->dateTime(),
-                Tables\Columns\TextColumn::make('user.name'),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime(),
+                //Tables\Columns\TextColumn::make('user.name'),
+               
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime(),
             ])
---
+++ laravel-10-blog-new/database/migrations/2024_02_03_093252_modify_published_at_column_in_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            DB::table('posts')->whereNull('published_at')->update(['published_at' => now()]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+};
